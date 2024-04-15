372675a94580e78b6d0d6fc906fdaf068b7f81bb
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2024-04-15 23:08:00 +0200

    Create ProjectZomboid_Server_Backup.bat

---
+++ ProjectZomboid_Server_Backup.bat
@@ -0,0 +1,26 @@
+@echo off
+echo ------------------------------------------------------------
+echo Backing up: "Modtest" server savegame folder...
+echo ------------------------------------------------------------
+echo **** %DATE% - %TIME% (Started)
+echo ------------------------------------------------------------
+cd "%userprofile%\Zomboid\Saves\Multiplayer"
+
+echo * Deleting old backup files...
+echo y | del backup.zip.old
+echo y | del backup.player.zip.old
+
+echo * Renaming previous backup files to .old (so you can revert to them).
+echo y | move backup.zip backup.zip.old
+echo y | move backup.player.zip backup.player.zip.old
+echo * Backing up world...
+cd Modtest
+7z a ..\backup.zip -r -mx1 >nul
+cd ..
+echo * Backing up localplayer profile (if any)...
+cd Modtest_player
+7z a ..\backup.player.zip -r -mx1 >nul
+echo ------------------------------------------------------------
+echo **** %DATE% - %TIME% (BACKUP DONE)
+echo ------------------------------------------------------------
+timeout 60
