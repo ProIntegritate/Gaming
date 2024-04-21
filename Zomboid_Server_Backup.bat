6e2904c9832d676242a29b61ab49033ac7521033
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2024-04-21 13:33:58 +0200

    Update Zomboid_Server_Backup.bat

--- Zomboid_Server_Backup.bat
+++ Zomboid_Server_Backup.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 rem Change the next line to the name of your server game name.
-set SERVERGAMENAME=test1
+set SERVERGAMENAME=MyServerGameName
 rem This script also requires 7zip somewhere in %PATH%.
 rem Either that or write the full path to 7Zip in this script.
 
@@ -13,31 +13,31 @@ echo ------------------------------------------------------------
 cd "%userprofile%\Zomboid\Saves\Multiplayer\"
 
 echo * Deleting .old backup files...
-echo y | del backup.zip.old
-echo y | del backup.player.zip.old
-echo y | del backup.serverconfig.zip.old
+echo y | del backup.%SERVERGAMENAME%.zip.old
+echo y | del backup.%SERVERGAMENAME%.player.zip.old
+echo y | del backup.%SERVERGAMENAME%.serverconfig.zip.old
 echo.
 
 echo * Renaming previous backup files to .old (so you can revert to them).
-echo y | move backup.zip backup.zip.old >nul
-echo y | move backup.player.zip backup.player.zip.old >nul
-echo y | move backup.serverconfig.zip backup.serverconfig.zip.old >nul
+echo y | move backup.%SERVERGAMENAME%.zip backup.%SERVERGAMENAME%.zip.old >nul
+echo y | move backup.%SERVERGAMENAME%.player.zip backup.%SERVERGAMENAME%.player.zip.old >nul
+echo y | move backup.%SERVERGAMENAME%.serverconfig.zip backup.%SERVERGAMENAME%.serverconfig.zip.old >nul
 echo.
 
 echo * Backing up world... (creating new backup files)
 cd %SERVERGAMENAME%
-7z a ..\backup.zip -r -mx1 >nul
+7z a ..\backup.%SERVERGAMENAME%.zip -r -mx1 >nul
 cd ..
 echo.
 
 echo * Backing up localplayer profile (if any)...
 cd %SERVERGAMENAME%_player
-7z a ..\backup.player.zip -r -mx1 >nul
+7z a ..\backup.%SERVERGAMENAME%.player.zip -r -mx1 >nul
 echo.
 
 echo * Backing up server config ...
 cd "%userprofile%\Zomboid\Server\"
-7z a %userprofile%\Zomboid\Saves\Multiplayer\backup.serverconfig.zip -r -mx1 %SERVERGAMENAME%* >nul
+7z a %userprofile%\Zomboid\Saves\Multiplayer\backup.%SERVERGAMENAME%.serverconfig.zip -r -mx1 %SERVERGAMENAME%* >nul
 echo.
 
 echo ------------------------------------------------------------
