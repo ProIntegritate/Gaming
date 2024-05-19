cfe11b17315c77b7c8754d571e9625f90da5b2aa
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2024-05-19 08:00:28 +0200

    Create callbacks_eventbased.lua

---
+++ Mods.ProjectZomboid/code/callbacks_eventbased.lua
@@ -0,0 +1,23 @@
+local function fOnPlayerUpdate()
+	-- This triggers CONSTANTLY, so be careful !!!
+end
+
+local function fOnZombieUpdate()
+	-- This triggers CONSTANTLY, so be careful !!!
+end
+
+local function fOnZombieDead()
+	-- Triggered when a zombie dies
+end
+local function fOnWeaponSwing()
+	-- Triggers when weapon is swinged/thrown
+end
+local function fOnHitZombie()
+	-- Triggers when Zombie is hit
+end
+
+Events.OnPlayerUpdate.Add(fOnPlayerUpdate)
+Events.OnZombieUpdate.Add(fOnZombieUpdate)
+Events.OnZombieDead.Add(fOnZombieDead)
+Events.OnWeaponSwing.Add(fOnWeaponSwing)
+Events.OnHitZombie.Add(fOnHitZombie)
