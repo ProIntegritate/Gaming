b994b95b987fb5f25033fd33874635ad23be33c3
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2024-05-23 17:10:05 +0200

    Update misc_code.lua

--- Mods.ProjectZomboid/code/misc_code.lua
+++ Mods.ProjectZomboid/code/misc_code.lua
@@ -82,7 +82,7 @@
 	-- you could do if A > 0 and A < 0, but that is just silly.
 
 
-	-- ******** Number of days since world creation ********
+	-- ******** Number of days since start of Zombie epidemic broke out ********
 	local daysSinceStart = math.floor(GameTime:getInstance():getWorldAgeHours() / 24);	-- Days since start.
 
 
